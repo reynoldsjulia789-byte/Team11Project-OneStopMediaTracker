cc92fd843ff4ca93d263f2133afd83a4cae247bc
Julia <reynolds.julia789@gmail.com>
2026-01-30 05:23:39 +0000

    Started InsertSampleData.sql
    TODO:
    - put sample data into tables

---
+++ InsertSampleData.sql
@@ -0,0 +1,67 @@
+-- this is our origional work no AI or external resources were used
+
+insert into Users (username, email)
+values            (),
+                  (),
+                  ();
+
+insert into MediaItems
+    (
+        mediaType,
+        title, 
+        releaseDate, 
+        runtimeMins, 
+        creator, 
+        platform
+    )
+values
+    (
+        'MOVIE',
+    ),
+    (
+        'SHOW',
+    ),
+    (
+        'BOOK',
+    );
+
+insert into SportsEvents
+    (
+        typeOfSport,
+        homeTeam,
+        awayTeam,
+        startTime,
+        runtimeMins,
+        recordingIsAvailable,
+        platform
+    )
+values
+    (
+        
+    ),
+    (
+        
+    ),
+    (
+
+    );
+
+insert into UserTrackerEntries
+    (
+        userID,
+        mediaItemID,
+        sportsEventID,
+        status,
+        savedAt,
+        completedAt
+    )
+values
+    (
+
+    ),
+    (
+        
+    ),
+    (
+
+    );
